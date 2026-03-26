2fc57fbf7f10a89b8ff45af3f7c07418b3bc1885
ShurieJr <shuuriyejr@gmail.com>
2026-03-26 10:05:38 +0300

    updated

--- src/AbstractExample/Main.java
+++ src/AbstractExample/Main.java
@@ -3,22 +3,17 @@ void main() {
 
     ProductCatalog catalog = new ProductCatalog();
 
-    Product laPatopBag = new Electronics(4 , "Labtop Bag-Jeep" , 20 , 5);
-
     Product laptop =
             new Electronics(1, "Laptop", 1000, 100);
 
     Product shirt =
             new Clothing(2, "T-Shirt", 40, 5);
 
-    Product book =
-            new Book(3, "Java Programming", 50, 0.10);
-    Product springBook= new Book(5, "Spring Book", 10, 0.05);
+    Product springBook=
+            new Book(3, "Spring Book", 20, 0.05);
 
     catalog.addProduct(laptop);
     catalog.addProduct(shirt);
-   catalog.addProduct(book);
-    catalog.addProduct(laPatopBag);
     catalog.addProduct(springBook);
 
     catalog.displayProducts();
--- src/InterfaceExample/Main.java
+++ src/InterfaceExample/Main.java
@@ -5,17 +5,18 @@ public class Main {
         CheckoutService checkout = new CheckoutService();
 
         PaymentMethod payment1 =
-                new CreditCardPayment("1234567890123456");
+                new CreditCardPayment("1234567890123458");
         checkout.checkout(payment1, 100);
         System.out.println("--------------");
 
-        PaymentMethod payment2 =
-                new PayPalPayment("customer@email.com");
-        checkout.checkout(payment2, 50);
-        System.out.println("--------------");
-        PaymentMethod payment3 =
-                new MobileMoneyPayment("615294858");
-        checkout.checkout(payment3, 90);
+        PaymentMethod mobileMoney = new MobileMoneyPayment("615294858");
+        checkout.checkout(mobileMoney, 100);
+        System.out.println("---------------");
+
+        PaymentMethod paypalMethod = new PayPalPayment("shuuriye@gmail.com");
+        checkout.checkout(paypalMethod, 45);
+
+
 
     }
 }
